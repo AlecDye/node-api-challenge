dc10f4c705d7334420bec70c9fc4995c5d544b24
Alec Dye <alecdye@gmail.com>
2020-04-10 10:21:45 -0700

    Adding GET actions endpoint

--- actionsRouter.js
+++ actionsRouter.js
@@ -7,10 +7,21 @@ const router = express.Router();
 // CRUD operations
 
 // GET action (to get a list of actions use the project-actions endpoint)
-router.get("/:id/actions/:id", (req, res) => {});
+router.get("/:id/actions/:id", (req, res) => {
+  const id = req.params.id;
+  Actions.get(id)
+    .then((action) => {
+      if (action) {
+        res.status(200).json({ message: "Requested action sent", action });
+      } else {
+        res.status(404).json({ message: "Action not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error, message: "Server broke" });
+    });
+});
 // POST actions
-//? validation working
-//! 500 error
 router.post("/:id/actions", (req, res) => {
   const newAction = req.body;
   if (!newAction.description || !newAction.notes) {
