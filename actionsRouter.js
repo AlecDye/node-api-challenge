34e4b0f59326cb4bc385946b09ab820be3f9a133
Alec Dye <alecdye@gmail.com>
2020-04-10 10:28:59 -0700

    Adding PUT and DELETE actions endpoints

--- actionsRouter.js
+++ actionsRouter.js
@@ -42,8 +42,37 @@ router.post("/:id/actions", (req, res) => {
 //! COME BACK TO THIS
 router.delete("/:id/actions/:id", (req, res) => {
   const id = req.params.id;
+  Actions.remove(id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ message: "Action deleted" });
+      } else {
+        res
+          .status(404)
+          .json({
+            message: "Action not found. You may have deleted it already",
+          });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error, message: "Server broke" });
+    });
 });
 // PUT actions
-router.put("/:id/actions/:id", (req, res) => {});
+router.put("/:id/actions/:id", (req, res) => {
+  const id = req.params.id;
+  const editAction = req.body;
+  if (!editAction.description || !editAction.notes) {
+    res.status(400).json({ message: "Description & Notes are required" });
+  } else {
+    Actions.update(id, editAction)
+      .then((action) => {
+        res.status(200).json({ message: "Action updated", action });
+      })
+      .catch((error) => {
+        res.status(500).json({ error, message: "Server broke" });
+      });
+  }
+});
 
 module.exports = router;
